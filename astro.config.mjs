042bf927747b7eb49030af3c6d7a0ead54066109
Vladimir <pukllaycreation@gmail.com>
2024-11-15 17:14:05 -0500

    actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pukllay.github.io",
-  base: "mockup",
+  site: "https://pukllay.github.io/mockup/",
+  base: "/mockup",
 });
